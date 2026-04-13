564e15aabafc33d420c77a58f8e5643fe1ea353f
gottadaniele <gotta.daniele@gmail.com>
2026-04-13 12:08:29 +0200

    Creato progetto versione Angular

---
+++ src/app/pages/pcto/pcto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pcto',
+  imports: [],
+  templateUrl: './pcto.component.html',
+  styleUrl: './pcto.component.css',
+})
+export class PctoComponent {
+
+}
---
+++ src/app/pages/richieste-ordini/richieste-ordini.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-richieste-ordini',
+  imports: [],
+  templateUrl: './richieste-ordini.component.html',
+  styleUrl: './richieste-ordini.component.css',
+})
+export class RichiesteOrdiniComponent {
+
+}
