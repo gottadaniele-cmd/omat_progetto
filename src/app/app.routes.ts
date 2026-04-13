cd87efbc25da489a02ccf9b24ccc28a82be967fa
gottadaniele <gotta.daniele@gmail.com>
2026-04-13 12:42:15 +0200

    aggiunti HTML e CSS, immagini e fonts del vecchio progetto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PctoComponent } from './pages/pcto/pcto.component';
+import { RichiesteOrdiniComponent } from './pages/richieste-ordini/richieste-ordini.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'pcto', component: PctoComponent },
+    { path: 'richieste-ordini', component: RichiesteOrdiniComponent }
+];
